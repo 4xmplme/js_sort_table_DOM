e1dfb9096ec6c8b72180bb0a6e0544a8a57f2230
4xmplme <andrii_kondratenko@icloud.com>
2024-07-05 14:25:57 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,30 @@
+"use strict";
+const tableColumns = [
+    ...document.querySelectorAll("table thead th")
+];
+const tableBody = document.querySelector("table tbody");
+const tableRows = [
+    ...tableBody.children
+];
+document.addEventListener("click", (e)=>{
+    const { target } = e;
+    if (tableColumns.includes(target)) {
+        const columnIndex = tableColumns.indexOf(target);
+        tableRows.sort((rowA, rowB)=>{
+            const { textContent: cellA } = rowA.children[columnIndex];
+            const { textContent: cellB } = rowB.children[columnIndex];
+            switch(target.textContent){
+                case "Name":
+                case "Position":
+                default:
+                    return cellA.localeCompare(cellB);
+                case "Age":
+                case "Salary":
+                    return +cellA.replace(/\D/g, "") - +cellB.replace(/\D/g, "");
+            }
+        });
+    }
+    tableBody.append(...tableRows);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
